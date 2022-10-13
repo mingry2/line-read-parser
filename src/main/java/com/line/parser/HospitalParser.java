66a641b67b316f6054c151ccffec5bf9bba09247
minkyoung kim <minry20415@naver.com>
2022-10-13 13:24:33 +0900

    [add] line-read-parser --mk

---
+++ src/main/java/com/line/parser/HospitalParser.java
@@ -0,0 +1,13 @@
+package com.line.parser;
+
+import com.line.domain.Hospital;
+
+public class HospitalParser implements Parser<Hospital>{
+
+    @Override
+    public Hospital parse(String str) {
+        str = str.replaceAll("\"", "");
+        String[] splitted = str.split(",");
+        return new Hospital(splitted[0], splitted[1]);
+    }
+}
